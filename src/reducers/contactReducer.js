eacffbdc504341b101adc2f01ded102e5acc84ce
rakibtg <rakibtg@gmail.com>
2018-07-06 23:37:14 +0600

    edit contact

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -2,11 +2,13 @@ import * as actionTypes from '../actions/actionTypes'
 
 export default ( state = [], action ) => {
     switch ( action.type ) {
+
         case actionTypes.CREATE_NEW_CONTACT:
             return [
                 ...state,
                 Object.assign( {}, action.contact )
             ]
+
         case actionTypes.REMOVE_CONTACT:
             return state.filter( ( data, i ) => {
                 if ( i !== action.id ) {
@@ -15,11 +17,13 @@ export default ( state = [], action ) => {
                     return false
                 }
             } )
+
         case actionTypes.EDIT_CONTACT:
-            // state.findIndex( action.contactIndex.index, n => {
-            //     console.log( n )
-            // } )
-            return state
+            console.log(action)
+            let statex = Object.assign( [], state )
+            statex[ action.contactIndex.index ][ 'name' ] = 'S'
+            return statex
+            
         default:
             return state
     }
