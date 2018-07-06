3efdd346c5d32695462183968a9cd6eba57a4d1b
rakibtg <rakibtg@gmail.com>
2018-07-06 23:36:48 +0600

    component states

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      name: ''
+      name: '',
+      editableIndex: null,
+      updatedName: '',
     }
   }
 
@@ -37,19 +39,50 @@ class App extends Component {
       <div key={index} className="row">
         <div className="col-md-10">
           <li key={index} className="list-group-item clearfix">
-            {data.name}
+            {
+              this.state.editableIndex === index ? (
+                <form>
+                  <input 
+                    value={ data.name }
+                    onChange={ e => {
+                      this.setState( {
+                        updatedName: e.target.value
+                      } )
+                    } }
+                  /> 
+                  <input 
+                    type='submit' 
+                    value='Update'
+                  />
+                </form>
+              ) : (
+                data.name
+              )
+            }
+            {/* {data.name} */}
           </li>
         </div>
         <div className="col-md-2">
-          <button onClick={(e) => this.editContact(e, index)} className="btn btn-danger">
+
+          <button onClick={(e) => this.editContactForm(e, index)} className="btn btn-danger">
             Edit
           </button>
+          {/* <button onClick={(e) => this.editContact(e, index)} className="btn btn-danger">
+            Edit
+          </button> */}
           <button onClick={(e) => this.deleteContact(e, index)} className="btn btn-danger">
             Remove
           </button>
         </div>
     </div> 
     )
+  }
+
+  editContactForm(e, index) {
+    e.preventDefault()
+    this.setState( {
+      editableIndex: index
+    } )
   }
 
   deleteContact(e, index){
@@ -65,7 +98,7 @@ class App extends Component {
   }
 
   render() {
-    let name;
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -93,15 +126,16 @@ class App extends Component {
 
 const mapStateToProps = ( state, ownProps ) => {
   return {
-    contacts: state.contacts
+    contacts: state.contacts,
+    editable: state.editable,
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    createContact: contact => dispatch( contactAction.createContact( contact ) ),
-    editContact  : contact => dispatch( contactAction.editContact( contact ) ),
-    deleteContact: index   => dispatch( contactAction.deleteContact( index ) )
+    createContact     : contact => dispatch( contactAction.createContact( contact ) ),
+    editContact       : contact => dispatch( contactAction.editContact( contact ) ),
+    deleteContact     : index   => dispatch( contactAction.deleteContact( index ) )
   }
 }
 
